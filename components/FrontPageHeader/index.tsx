a804b11b25df6389887aa866738e27a5f594d61c
Angel <22824872+Zyplos@users.noreply.github.com>
2023-12-06 21:49:00 -0600

    text wall done!

--- components/FrontPageHeader/index.tsx
+++ components/FrontPageHeader/index.tsx
@@ -8,7 +8,7 @@ export default function FrontPageHeader() {
   const [DEV_CHANGER_SPEED, SET_DEV_CHANGER_SPEED] = useState(1);
 
   useEffect(() => {
-    console.log("CANVASSTART======================================");
+    // console.log("CANVASSTART======================================");
     // ===== CANVAS SETUP
     const canvas = canvasRef.current;
     if (!canvas) return;
@@ -16,7 +16,7 @@ export default function FrontPageHeader() {
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    let FRAME = 0;
+    // let FRAME = 0;
     let animationFrameId: number;
 
     let WIDTH = canvas.width;
@@ -25,14 +25,12 @@ export default function FrontPageHeader() {
     const BAR_HEIGHT = 75;
     const FONT_SIZE = Math.floor(BAR_HEIGHT * 0.5);
     const MOVE_STEP = 0.5;
-    // const DEV_SPEEDREF = speedRef.current;
-    // let MOVE_STEP = DEV_SPEEDREF ? parseInt(DEV_SPEEDREF.value) : 2;
-    // // on input change
-    // if (DEV_SPEEDREF) {
-    //   DEV_SPEEDREF.addEventListener("input", (e: any) => {
-    //     MOVE_STEP = parseInt(e?.target?.value ?? 2);
-    //   });
-    // }
+
+    const TEXT_COLOR = "#1e1e1e";
+    const FONT_STYLE = "bold " + FONT_SIZE + "px sans-serif";
+
+    const EVEN_BAR_COLOR = "#050505";
+    const ODD_BAR_COLOR = "#090909";
 
     /*
       couple of quirks with this function:
@@ -42,8 +40,8 @@ export default function FrontPageHeader() {
       so it gets called right at the beginning of all the drawing code
     */
     const setupCanvas = () => {
-      ctx.fillStyle = "#ffd300";
-      ctx.font = FONT_SIZE + "px sans-serif";
+      ctx.fillStyle = TEXT_COLOR;
+      ctx.font = FONT_STYLE;
       ctx.textBaseline = "middle";
     };
 
@@ -58,7 +56,7 @@ export default function FrontPageHeader() {
       // so we need to set them again
       setupCanvas();
 
-      console.log("resized canvas to", WIDTH, HEIGHT);
+      // console.log("resized canvas to", WIDTH, HEIGHT);
     };
     resizeCanvas();
     window.addEventListener("resize", resizeCanvas);
@@ -74,74 +72,15 @@ export default function FrontPageHeader() {
     };
 
     // const BAR_WIDTH = WIDTH < 1000 ? 1500 : WIDTH + Math.floor(WIDTH * 0.33);
-    const BAR_WIDTH = HEIGHT + Math.floor(HEIGHT * 0.7);
+    // const BAR_WIDTH = HEIGHT + Math.floor(HEIGHT * 0.7);
+    const BAR_WIDTH = HEIGHT * 2;
     // const BAR_WIDTH = 400;
-
-    let DEV_XSTART = 0;
-    let DEV_YSTART = HEIGHT;
-
-    const drawBar = (number: number, x: number, y: number) => {
-      const calcedWidth = WIDTH < 1000 ? 1500 : WIDTH + Math.floor(WIDTH * 0.33);
-      ctx.save();
-
-      ctx.fillStyle = number % 2 === 0 ? "#ff3e3e" : "#ffd300";
-      // ctx.fillStyle = number % 2 === 0 ? "#0a0a0a" : "#050505";
-
-      // set anchor point to bottom left
-      ctx.translate(x, BAR_HEIGHT);
-
-      ctx.rotate((-45 * Math.PI) / 180);
-      ctx.fillRect(x, y, calcedWidth, BAR_HEIGHT);
-
-      // ===== TEXTDEV
-
-      ctx.save();
-
-      ctx.fillStyle = "#fff";
-      ctx.font = FONT_SIZE + "px sans-serif";
-
-      // ctx.textAlign = "center";
-      ctx.textBaseline = "top";
-      ctx.translate(DEV_XSTART, DEV_YSTART);
-      // ctx.rotate((-45 * Math.PI) / 180);
-      ctx.fillText("Scrollable text", 0, 0 + BAR_HEIGHT / 2 - FONT_SIZE / 2);
-
-      DEV_XSTART += MOVE_STEP;
-
-      if (DEV_YSTART < 0) {
-        DEV_XSTART = 0;
-        DEV_YSTART = HEIGHT;
-      }
-
-      ctx.restore();
-
-      // ===== TEXTDEV
-
-      ctx.restore();
-    };
 
     const calcTextWidth = (text: string) => {
       return Math.floor(ctx.measureText(text).width);
     };
 
-    // textInstances.push(new ScrollingText("Scrollable text!!!!!!!!!!!", DEV_XSTART, HEIGHT + calcTextWidth("Scrollable text!!!!!!!!!!!")));
-    // textInstances.push(new ScrollingText("backwards scrollable", DEV_XSTART + 560, 20, true));
-
-    // const createNewTextInstance = (text: string, x: number, y:number, backward: boolean =  false) => {
-    //   textInstances.push(new ScrollingText(text, x, y, backward));
-    // }
-
-    const TEXT_SPACER = 110;
-    const SPACER_CHAR = " - ";
-
-    // textInstances.push(new ScrollingText("Scrollable text!!!!!!!!!!!" + SPACER_CHAR, TEXT_STARTER_ORIGIN, HEIGHT));
-    // textInstances.push(
-    //   new ScrollingText(
-    //     "backwards scrollable" + SPACER_CHAR,
-    //     TEXT_STARTER_ORIGIN - calcTextWidth("backwards scrollable" + SPACER_CHAR) + TEXT_SPACER,
-    //     HEIGHT + calcTextWidth("backwards scrollable" + SPACER_CHAR) - TEXT_SPACER
-    //   )
-    // );
+    const SPACER_CHAR = " • ";
 
     // each bar is offset by this value (the 4 is error correction)
     const BAR_OFFSET = Math.floor(Math.cos(45) * BAR_HEIGHT) + 4;
@@ -156,10 +95,9 @@ export default function FrontPageHeader() {
     const MAX_BARS = maxBarsWithEmptySpace + Math.floor(maxBarsWithEmptySpace / 2);
 
     // const RANDOM_STRINGS = ["Scrollable text!!!!", "sample text", "shoutouts"];
-    // const RANDOM_STRINGS = ["test string", "scrollable", "plinko", "wow", "me", "typical string"];
-    const RANDOM_STRINGS = ["plinko"];
-    // const RANDOM_STRINGS = ["sample text"];
-    // const RANDOM_STRINGS = ["WWW"];
+    const RANDOM_STRINGS = ["test string", "scrollable", "plinko", "wow", "me", "particularly long string"];
+    // const RANDOM_STRINGS = ["plinko"];
+
     const getRandomString = () => {
       return RANDOM_STRINGS[Math.floor(Math.random() * RANDOM_STRINGS.length)] + SPACER_CHAR;
     };
@@ -172,7 +110,6 @@ export default function FrontPageHeader() {
       yCounter: number;
 
       constructor(text: string, x: number, y: number, backward: boolean = false, startPos?: number) {
-        // console.table({ text, x, y, backward, startPos, calcTextWidth: calcTextWidth(text) });
         this.text = text;
 
         if (!backward) {
@@ -187,27 +124,20 @@ export default function FrontPageHeader() {
         this.backward = backward;
 
         this.yCounter = 0;
+        // this is for the initial wall text which needs its yCounter already set
         if (startPos) {
-          if (!backward) {
-            this.yCounter = startPos;
-          } else {
-            this.yCounter = startPos;
-          }
+          this.yCounter = startPos;
         }
       }
 
       draw(ctx: CanvasRenderingContext2D) {
-        // ===== TEXTDEV
-
         ctx.save();
 
-        ctx.fillStyle = "#fff";
-        ctx.font = FONT_SIZE + "px sans-serif";
+        ctx.fillStyle = TEXT_COLOR;
+        ctx.font = FONT_STYLE;
 
-        // ctx.textAlign = "center";
         ctx.textBaseline = "top";
         ctx.translate(this.x, this.y);
-        // ctx.rotate((-45 * Math.PI) / 180);
         ctx.fillText(this.text, 0, 0 + BAR_HEIGHT / 2 - FONT_SIZE / 2);
 
         if (!this.backward) {
@@ -218,17 +148,14 @@ export default function FrontPageHeader() {
           this.yCounter += MOVE_STEP;
         }
 
-        // console.log("this.yCounter", this.yCounter);
-
         ctx.restore();
-
-        // ===== TEXTDEV
       }
 
       destroyable() {
         // text width is added so it gets destroyed after it leaves the bar
-        // this is multipled by two to hide a bug where the last item flashes and shifts a bit after something gets deleted
-        if (this.yCounter > BAR_WIDTH + calcTextWidth(this.text) * 2) {
+        // added a bit more offset than the text's width
+        // to hide a bug where the last item flashes and shifts a bit after something gets deleted
+        if (this.yCounter > BAR_WIDTH + calcTextWidth(this.text) * 4) {
           return true;
         }
       }
@@ -250,8 +177,7 @@ export default function FrontPageHeader() {
       drawBar(ctx: CanvasRenderingContext2D) {
         ctx.save();
 
-        ctx.fillStyle = this.number % 2 === 0 ? "#ff3e3e" : "#aa0000";
-        // ctx.fillStyle = number % 2 === 0 ? "#0a0a0a" : "#050505";
+        ctx.fillStyle = this.number % 2 === 0 ? EVEN_BAR_COLOR : ODD_BAR_COLOR;
 
         // set anchor point to bottom left
         ctx.translate(this.x, BAR_HEIGHT);
@@ -264,29 +190,12 @@ export default function FrontPageHeader() {
         ctx.restore();
       }
 
-      textManager() {
-        if (this.textInstances.length === 0) {
-          const backward = this.number % 2 === 0 ? true : false;
-          // create new text instances for empty arrays
-          // this.textInstances.push(new BarTextInstance(getRandomString(), this.x, this.y, backward));
-          // reload page
-          // window.location.reload();
-        }
-      }
-
       drawText(ctx: CanvasRenderingContext2D) {
         this.textInstances.forEach((textInstance, index) => {
           textInstance.draw(ctx);
 
           if (textInstance.destroyable()) {
             this.textInstances.splice(index, 1);
-            // weird bug where new last item is shifted a few pixels
-            // add a few pixels to compensate
-
-            // const newLastItem = this.textInstances[0];
-            // if (!newLastItem) return;
-            // newLastItem.yCounter += 2;
-            // newLastItem.x += 2;
           }
 
           // the last text instance is the first one in line
@@ -303,32 +212,21 @@ export default function FrontPageHeader() {
 
       draw(ctx: CanvasRenderingContext2D) {
         this.drawBar(ctx);
-        this.textManager();
       }
     }
 
-    const DEV_BARINSTANCES: BarTextCombo[] = [];
-    // DEV_BARINSTANCES.push(new BarTextCombo(0, 0, HEIGHT));
-    // DEV_BARINSTANCES[0].textInstances.push(new BarTextInstance("wowow", 0, HEIGHT));
+    const BAR_INSTANCES: BarTextCombo[] = [];
 
-    const DEV_BAR_ID = 7;
+    // const DEV_BAR_ID = 7;
     for (let i = 0; i < MAX_BARS; i++) {
       // if (i !== DEV_BAR_ID) continue;
       const isBackward = i % 2 === 0 ? true : false;
-      console.log("isBackward", isBackward, "i", i);
+
       // y has some extra offset to make up for the empty space at the bottom
       const devXBar = X_ORIGIN + BAR_OFFSET * i;
       const devYBar = Math.floor(BAR_HEIGHT * 2) + X_ORIGIN + HEIGHT + BAR_OFFSET * i;
-      // console.log("devXBar", devXBar, "devYBar", devYBar);
 
       const newBar = new BarTextCombo(i, devXBar, devYBar);
-      // const DEV_WORDS = ["0|wow", "1|reallylongword", "2|plinko"];
-      const DEV_WORDS = ["0|wow", "1|plinko", "2|reallylongword"];
-
-      // newBar.textInstances.push(new BarTextInstance(DEV_WORDS[0], devXBar, devYBar, isBackward));
-      // const DEV_SELECTED_WORD = DEV_WORDS[1];
-      // const DEV_SELECTED_WORD_WIDTH = calcTextWidth(DEV_SELECTED_WORD);
-      // newBar.textInstances.push(new BarTextInstance(DEV_SELECTED_WORD, devXBar + DEV_SELECTED_WORD_WIDTH, devYBar, isBackward, DEV_SELECTED_WORD_WIDTH));
 
       let currentWidthTaken = 0;
       let DEV_MAX_COUNTER = 0;
@@ -354,169 +252,22 @@ export default function FrontPageHeader() {
       // so reverse the array so its ready for the text manager
       newBar.textInstances.reverse();
 
-      DEV_BARINSTANCES.push(newBar);
+      BAR_INSTANCES.push(newBar);
     }
 
-    let DEV_LASTFRAME = 0;
-    const drawDev = (frameCount: number) => {
-      const i = 20;
-      // const devXBar = X_ORIGIN + BAR_OFFSET * i;
-      // const devYBar = Math.floor(BAR_HEIGHT * 2) + X_ORIGIN + HEIGHT + BAR_OFFSET * i;
-      // drawBar(0, 0, HEIGHT);
-
-      // const devBar = DEV_BARINSTANCES[0];
-      // devBar.draw(ctx);
-
-      DEV_BARINSTANCES.forEach((devBar, index) => {
+    const drawDev = () => {
+      BAR_INSTANCES.forEach((devBar, index) => {
         // dont draw odd bars
         // if (index % 2 !== 1) return;
         devBar.draw(ctx);
       });
-
-      const devBar = DEV_BARINSTANCES[0];
-      let devBarInstance = devBar.textInstances[1];
-      const WEIRD_FRAME_HAPPENING = devBarInstance?.yCounter - DEV_LASTFRAME !== 2;
-      if (WEIRD_FRAME_HAPPENING) {
-        devBarInstance = devBar.textInstances[2];
-      } else {
-        devBarInstance = devBar.textInstances[1];
-      }
-      // TODO WEIRD FLASHING PROBLEM WHERE LAST ITEM SHIFTS
-      if (DEV_LASTFRAME !== devBarInstance?.yCounter + 1) {
-        // console.log(
-        //   "!!!!!!!!!!!!!FRAMEDIFF | ",
-        //   "DEV_LASTFRAME",
-        //   DEV_LASTFRAME,
-        //   " | yCounter",
-        //   devBarInstance?.yCounter,
-        //   " | DIFF ",
-        //   devBarInstance?.yCounter - DEV_LASTFRAME
-        // );
-      }
-
-      // ctx.fillText(`X ${DEV_XSTART} | Y ${DEV_YSTART}`, WIDTH / 2, HEIGHT / 2);
-      // ctx.fillText(
-      //   `insts ${devBar.textInstances.length} | LOOKING AT [${devBarInstance.text}] | X ${devBarInstance?.x} | YCOUNTER ${devBarInstance?.yCounter}`,
-      //   10,
-      //   HEIGHT / 2
-      // );
-
-      DEV_LASTFRAME = devBarInstance?.yCounter;
     };
 
-    type TextMap = ScrollingText[][];
-    const TEXT_MAP: TextMap = [];
-    // create an array for all bars
-    for (let i = 0; i < MAX_BARS; i++) {
-      TEXT_MAP[i] = [];
-    }
-
-    class ScrollingText {
-      text: string;
-      x: number;
-      y: number;
-      backward: boolean;
-
-      constructor(text: string, x: number, y: number, backward: boolean = false) {
-        this.text = text;
-        this.x = x;
-        this.y = y;
-        this.backward = backward;
-      }
-
-      draw(ctx: CanvasRenderingContext2D) {
-        ctx.save();
-
-        ctx.fillStyle = "#fff";
-        ctx.font = FONT_SIZE + "px sans-serif";
-
-        ctx.translate(this.x, this.y);
-        ctx.rotate((-45 * Math.PI) / 180);
-        ctx.fillText(this.text, 0, 0);
-
-        if (!this.backward) {
-          this.x += MOVE_STEP;
-          this.y -= MOVE_STEP;
-        }
-
-        if (this.backward) {
-          this.x -= MOVE_STEP;
-          this.y += MOVE_STEP;
-        }
-
-        ctx.restore();
-      }
-
-      destroyable() {
-        if (!this.backward) {
-          if (this.y < 0) {
-            return true;
-          }
-        }
-
-        if (this.backward) {
-          if (this.y > HEIGHT + calcTextWidth(this.text)) {
-            return true;
-          }
-        }
-      }
-    }
-
-    const drawBars = (frameCount: number) => {
-      // console.log(textInstances);
-      // show frame count
-      ctx.fillText(`${frameCount.toString()}`, WIDTH / 2, HEIGHT / 2);
-
-      for (let i = 0; i < MAX_BARS; i++) {
-        // y has some extra offset to make up for the empty space at the bottom
-        const devXBar = X_ORIGIN + BAR_OFFSET * i;
-        const devYBar = Math.floor(BAR_HEIGHT * 2) + X_ORIGIN + HEIGHT + BAR_OFFSET * i;
-        // console.log("devXBar", devXBar, "devYBar", devYBar);
-        drawBar(i, devXBar, devYBar);
-      }
-    };
-
-    // const X_ORIGIN_TEXT = -BAR_HEIGHT * 7 - 13;
-    const X_ORIGIN_TEXT = 0;
-    console.log("X_ORIGIN_TEXT", X_ORIGIN_TEXT);
-
-    const TEXT_OFFSET = BAR_OFFSET + 10;
-
-    console.log("TEXT INSTANCES", TEXT_MAP);
-    const drawText = (frameCount: number) => {
-      TEXT_MAP.forEach((barArray, i) => {
-        // if (i > 20 || i < 10) return;
-        // if (![20].includes(i)) return;
-        if (barArray.length === 0) {
-          // create new text instances for empty arrays
-          const newString = i + "|" + getRandomString() + SPACER_CHAR;
-          const devWorkingX = X_ORIGIN_TEXT + TEXT_OFFSET * i - calcTextWidth(newString);
-          const devWorkingY = 10 + X_ORIGIN_TEXT + TEXT_OFFSET * i + HEIGHT + calcTextWidth(newString);
-
-          const devXBar = X_ORIGIN + BAR_OFFSET * i;
-          const devYBar = Math.floor(BAR_HEIGHT * 2) + X_ORIGIN + HEIGHT + BAR_OFFSET * i;
-
-          console.log("X_ORIGIN_TEXT", X_ORIGIN_TEXT, "devWorkingX", devWorkingX, "devWorkingY", devWorkingY);
-          TEXT_MAP[i].push(new ScrollingText(newString, devWorkingX, devWorkingY));
-          // TEXT_MAP[i].push(new ScrollingText(newString, devXBar, devYBar));
-        } else {
-          TEXT_MAP[i].forEach((textInstance, j) => {
-            textInstance.draw(ctx);
-
-            if (textInstance.destroyable()) {
-              TEXT_MAP[i].splice(j, 1);
-            }
-          });
-        }
-      });
-    };
     // ===== RENDER LOOP
     const render = () => {
-      FRAME++;
+      // FRAME++;
       drawBg();
-      // drawBars(FRAME);
-      // drawText(FRAME);
-      drawDev(FRAME);
+      drawDev();
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
@@ -524,9 +275,6 @@ export default function FrontPageHeader() {
     return () => {
       window.cancelAnimationFrame(animationFrameId);
       window.removeEventListener("resize", resizeCanvas);
-      // DEV_SPEEDREF?.removeEventListener("input", (e: any) => {
-      //   MOVE_STEP = parseInt(e?.target?.value ?? 2);
-      // });
     };
   }, [DEV_CHANGER_SPEED]);
 
@@ -535,8 +283,6 @@ export default function FrontPageHeader() {
       <header className={styles.wrapper}>
         <canvas className={styles.canvas} ref={canvasRef}></canvas>
       </header>
-      {/* <input type="range" min="0" max="4" ref={speedRef} value={DEV_CHANGER_SPEED} onChange={(e) => SET_DEV_CHANGER_SPEED(parseInt(e.target.value))} /> */}
-      <input type="range" min="0" max="4" ref={speedRef} />
     </>
   );
 }
