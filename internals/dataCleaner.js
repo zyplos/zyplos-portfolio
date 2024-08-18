26b2b2a37904c2286f112c91eb4bfd1b2b7a9631
Angel <22824872+Zyplos@users.noreply.github.com>
2024-08-18 05:36:16 -0500

    prioritize my colors in colorData

--- internals/dataCleaner.js
+++ internals/dataCleaner.js
@@ -24,6 +24,8 @@ function rgbToHex(red, green, blue) {
 
 const checking = false;
 
+const zyColors = [];
+
 for (const colorData of data) {
 	const reps = colorData.representations;
 
@@ -55,15 +57,54 @@ for (const colorData of data) {
 	const hex = rgbToHex(r, g, b);
 	console.log(colorData.name.padEnd(30, " "), hex);
 
-	newData.push({
-		name: colorData.name,
-		createdDate: colorData.created_date,
-		modifiedDate: colorData.modified_date,
-		colorProfile: repColorData.profileName,
-		hex,
-		rgb: { r, g, b },
-	});
+	// check if name starts with "Zyplos"
+	if (colorData.name.startsWith("Zyplos")) {
+		zyColors.push({
+			name: colorData.name,
+			// createdDate: colorData.created_date,
+			// modifiedDate: colorData.modified_date,
+			// colorProfile: repColorData.profileName,
+			hex,
+			rgb: { r, g, b },
+		});
+	} else {
+		newData.push({
+			name: colorData.name,
+			// createdDate: colorData.created_date,
+			// modifiedDate: colorData.modified_date,
+			// colorProfile: repColorData.profileName,
+			hex,
+			rgb: { r, g, b },
+		});
+	}
 }
+
+const zyColorOrder = [
+	"Red",
+	"Yellow",
+	"Gray",
+	"Dark Gray",
+	"Slate",
+	"Deepslate",
+	"Bedrock",
+	"Green",
+];
+
+// reverse order
+zyColorOrder.reverse();
+
+for (const orderColor of zyColorOrder) {
+	console.log("orderColor", orderColor);
+	// find the color in the zyColors array and remove it
+	const foundColor = zyColors.find((color) => color.name.includes(orderColor));
+	if (foundColor) {
+		newData.unshift(foundColor);
+		zyColors.splice(zyColors.indexOf(foundColor), 1);
+	}
+}
+
+// add the rest of the zyColors after the index of the first color in the zyColorOrder in newData
+newData.splice(zyColorOrder.length, 0, ...zyColors);
 
 // save to colorData.json
 const fs = require("node:fs");
