8c120559cd79431ea3eaedddbb7ed9988b533bba
Angel <22824872+Zyplos@users.noreply.github.com>
2025-05-01 16:51:58 -0500

    now isnt this nice

---
+++ pages/now-isnt-this-nice.tsx
@@ -0,0 +1,22 @@
+import Fullscreen from "@/components/Fullscreen";
+import Head from "next/head";
+import img from "@/assets/now-isnt-this-nice.jpg";
+
+export default function StatuePage() {
+	return (
+		<>
+			<Head>
+				<title>⬤</title>
+			</Head>
+			<Fullscreen
+				style={{
+					backgroundColor: "#b6cde6",
+					userSelect: "none",
+					padding: "3rem",
+				}}
+			>
+				<img src={img.src} alt="Now isn't this nice?" />
+			</Fullscreen>
+		</>
+	);
+}
