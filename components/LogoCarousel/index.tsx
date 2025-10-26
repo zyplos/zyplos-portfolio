083b976a9e8138d9339f2dcf03b0ffab2b3eb05c
Angel <22824872+Zyplos@users.noreply.github.com>
2025-10-26 16:54:31 -0500

    working LogoCarousel from kreudev/react-infinite-logo-slider

---
+++ components/LogoCarousel/index.tsx
@@ -0,0 +1,102 @@
+// adapted from https://github.com/kreudev/react-infinite-logo-slider
+import React from "react";
+import styles from "./styles.module.scss";
+
+export interface LogoCarouselProps {
+  children: React.ReactElement[];
+  width?: string;
+  duration?: number;
+  toRight?: boolean;
+  pauseOnHover?: boolean;
+  blurBorders?: boolean;
+  blurBorderColor?: string;
+}
+
+export interface SlideProps {
+  children: React.ReactNode;
+  width?: string;
+}
+
+const LogoCarousel: React.FC<LogoCarouselProps> & {
+  Slide: React.FC<SlideProps>;
+} = ({
+  children,
+  width = "200px",
+  duration = 40,
+  toRight = false,
+  pauseOnHover = false,
+  blurBorders = false,
+  blurBorderColor = "#fff",
+}) => {
+  const sliderStyle = {
+    "--slide-width": width,
+    "--children-length": children.length,
+    "--duration": `${duration}s`,
+  } as React.CSSProperties;
+
+  const blurStyle = {
+    "--blur-border-color": blurBorderColor,
+  } as React.CSSProperties;
+
+  return (
+    <div className={styles.logoCarousel}>
+      <div
+        className={`${styles.wrapper} ${pauseOnHover ? styles.pauseOnHover : ""}`}
+      >
+        <div
+          className={`${styles.slider} ${toRight ? styles.toRight : ""}`}
+          style={sliderStyle}
+        >
+          {children.map((child, i) => (
+            <React.Fragment key={`one-${i}`}>
+              {React.cloneElement(child, { width })}
+            </React.Fragment>
+          ))}
+          {children.map((child, i) => (
+            <React.Fragment key={`two-${i}`}>
+              {React.cloneElement(child, { width })}
+            </React.Fragment>
+          ))}
+          {children.map((child, i) => (
+            <React.Fragment key={`three-${i}`}>
+              {React.cloneElement(child, { width })}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+
+      {blurBorders && (
+        <>
+          <div
+            className={`${styles.blurBorders} ${styles.left}`}
+            style={blurStyle}
+          />
+          <div
+            className={`${styles.blurBorders} ${styles.right}`}
+            style={blurStyle}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+const Slide: React.FC<SlideProps> = ({
+  children,
+  width = "200px",
+  ...props
+}) => {
+  return (
+    <div
+      className={styles.slide}
+      style={{ "--slide-width": width } as React.CSSProperties}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+LogoCarousel.Slide = Slide;
+
+export default LogoCarousel;
