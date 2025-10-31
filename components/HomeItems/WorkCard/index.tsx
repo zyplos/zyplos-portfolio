8d5a0502aaed3ae988bc3e4c31fc800db4d07a1f
Angel <22824872+Zyplos@users.noreply.github.com>
2025-10-30 22:04:43 -0500

    fix up looking for work card

--- components/HomeItems/WorkCard/index.tsx
+++ components/HomeItems/WorkCard/index.tsx
@@ -1,25 +1,20 @@
 import clsx from "clsx";
-import styles from "./styles.module.scss";
+import HomeCard from "../HomeCard";
 
 interface WorkCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export default function WorkCard({ ...props }: WorkCardProps) {
+export default function WorkCard({ className, ...props }: WorkCardProps) {
   return (
-    <div
-      {...props}
-      className={clsx(styles.card, styles.workCard, props.className)}
-    >
-      <h2 className={styles.workTitle}>{"i'm looking for work!"}</h2>
-      <p className={styles.bioText}>
-        {
-          "i'm a developer who's worked with the web and various other things for many years now"
-        }
+    <HomeCard {...props} className={clsx("textContent", className)}>
+      <h2>i'm looking for work!</h2>
+      <p>
+        i'm a developer who's worked with the web and various other things for
+        many years now
       </p>
-      <p className={styles.bioText}>
-        {
-          "i like spending my time making stuff and i'm always happy to learn something new. come see some of my stuff on this site"
-        }
+      <p>
+        i like spending my time making stuff and i'm always happy to learn
+        something new. come see some of my stuff on this site
       </p>
-    </div>
+    </HomeCard>
   );
 }
