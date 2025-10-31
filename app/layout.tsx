5a56b244e9e331adc3305de670867ed089b3a58b
Angel <22824872+Zyplos@users.noreply.github.com>
2025-10-31 18:06:51 -0500

    made changes according to nextjs 16's update docs

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
       <body>
