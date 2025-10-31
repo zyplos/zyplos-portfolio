619b99bfaae7e1632b3fcd0ec30935302893825b
Angel <22824872+Zyplos@users.noreply.github.com>
2025-10-30 22:07:37 -0500

    fix up bio card

--- components/HomeItems/BioCard/index.tsx
+++ components/HomeItems/BioCard/index.tsx
@@ -1,22 +1,20 @@
 import clsx from "clsx";
-import styles from "./styles.module.scss";
+import HomeCard from "../HomeCard";
 
 interface BioCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function BioCard({ ...props }: BioCardProps) {
   return (
-    <div {...props} className={clsx(styles.card, props.className)}>
-      <h1 className={styles.bioTitle}>programmer, designer</h1>
-      <p className={styles.bioText}>
-        {
-          "i'm a developer who's worked with the web and various other things for many years now"
-        }
+    <HomeCard {...props} className={clsx("textContent", props.className)}>
+      <h1>programmer, designer</h1>
+      <p>
+        i'm a developer who's worked with the web and various other things for
+        many years now
       </p>
-      <p className={styles.bioText}>
-        {
-          "i like spending my time making stuff and i'm always happy to learn something new. come see some of my stuff on this site"
-        }
+      <p>
+        i like spending my time making stuff and i'm always happy to learn
+        something new. come see some of my stuff on this site
       </p>
-    </div>
+    </HomeCard>
   );
 }
