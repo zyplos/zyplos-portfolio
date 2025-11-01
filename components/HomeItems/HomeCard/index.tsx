9f8d39e6d10e60813e65e4f012dae0c5bef1563e
Angel <22824872+Zyplos@users.noreply.github.com>
2025-10-31 23:29:15 -0500

    home cards can take all different padding variants now

--- components/HomeItems/HomeCard/index.tsx
+++ components/HomeItems/HomeCard/index.tsx
@@ -1,19 +1,21 @@
 import clsx from "clsx";
 import styles from "./styles.module.scss";
 
+type Padding = "xxs" | "xs" | "s" | "m" | "l" | "xl" | "none";
+
 interface HomeCardProps extends React.HTMLAttributes<HTMLDivElement> {
-  noPadding?: boolean;
+  padding?: Padding;
 }
 
 export default function HomeCard({
   className,
   children,
-  noPadding,
+  padding = "l",
   ...props
 }: HomeCardProps) {
   return (
     <div
-      className={clsx(styles.card, noPadding && styles.noPadding, className)}
+      className={clsx(styles.card, styles[`padding-${padding}`], className)}
       {...props}
     >
       {children}
