6281e05d3e8af9ed6b6291f966f2f1489fadd613
Angel <22824872+Zyplos@users.noreply.github.com>
2025-11-02 14:32:08 -0600

    reuse old Discord status card for new one

--- components/HomeItems/DiscordStatusCard/index.tsx
+++ components/HomeItems/DiscordStatusCard/index.tsx
@@ -1,30 +1,84 @@
+"use server";
+import Image from "next/image";
 import clsx from "clsx";
 import { HomeCard, SmallHeading } from "../HomeCard";
+import getDiscordPresence from "@/internals/getDiscordPresence";
 import styles from "./styles.module.scss";
+
+const friendlyStatusTitle = {
+  online: "⬤ Currently online",
+  offline: "🞮 Currently offline",
+  idle: "* Taking a break",
+  dnd: "🞓 Busy working",
+};
 
 interface DiscordStatusCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export default function DiscordStatusCard({
+export default async function DiscordStatusCard({
   className,
   ...props
 }: DiscordStatusCardProps) {
+  const data = await getDiscordPresence();
+
+  if (data.status === "offline") {
+    return (
+      <>
+        <p className={clsx(styles.statusText, styles.offline)}>
+          {friendlyStatusTitle.offline}
+        </p>
+        <span>not working online on anything at the moment</span>
+      </>
+    );
+  }
+
+  const presence = data.presence;
+
+  const statusText = friendlyStatusTitle[data.status];
+
   return (
     <HomeCard
       {...props}
       className={clsx(styles.cardWrapper, className)}
       padding="m"
     >
-      <SmallHeading>Working in Visual Studio Code</SmallHeading>
-      <div className={styles.vscodeContent}>
-        <div className={styles.vscodeIconWrapper}>
-          <div className={styles.vscodeIcon} />
-          <div className={styles.vscodeStatusDot} />
-        </div>
-        <div className={styles.vscodeInfo}>
-          <p className={styles.vscodeDetail}>Editing: index.tsx</p>
-          <p className={styles.vscodeDetail}>Workspace: localhost</p>
-        </div>
-      </div>
+      <SmallHeading className={clsx(styles.statusText, styles[data.status])}>
+        {statusText}
+      </SmallHeading>
+      {presence && (
+        <>
+          <p className={styles.gameText}>Working in {presence.name}</p>
+          <div className={styles.richPresence}>
+            <div className={styles.images}>
+              {presence.largeImageUrl && (
+                <Image
+                  src={presence.largeImageUrl}
+                  alt={presence.largeImageAlt ?? ""}
+                  title={presence.largeImageAlt ?? ""}
+                  priority
+                  width={128}
+                  height={128}
+                  className={styles.largeImage}
+                />
+              )}
+              {presence.smallImageUrl && (
+                <Image
+                  src={presence.smallImageUrl}
+                  alt={presence.smallImageAlt ?? ""}
+                  title={presence.smallImageAlt ?? ""}
+                  priority
+                  width={128}
+                  height={128}
+                  className={styles.smallImage}
+                />
+              )}
+            </div>
+            <div>
+              {presence.details && <p>{presence.details}</p>}
+              {presence.state && <p>{presence.state}</p>}
+            </div>
+          </div>
+        </>
+      )}
     </HomeCard>
   );
 }
