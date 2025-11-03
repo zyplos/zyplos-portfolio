918edddf6e8742113b0b5edbf663e989f6ccbb7a
Angel <22824872+Zyplos@users.noreply.github.com>
2025-11-03 00:08:06 -0600

    new commit component ui done

--- components/HomeItems/LatestCommitCard/index.tsx
+++ components/HomeItems/LatestCommitCard/index.tsx
@@ -1,25 +1,34 @@
+"use client";
+
 import clsx from "clsx";
+import { HomeCard, SmallHeading } from "../HomeCard";
 import styles from "./styles.module.scss";
+import Folder from "@/components/Icons/Folder";
+import Branch from "@/components/Icons/Branch";
 
 interface LatestCommitCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export default function LatestCommitCard({ ...props }: LatestCommitCardProps) {
+export default function LatestCommitCard({
+  className,
+  ...props
+}: LatestCommitCardProps) {
   return (
-    <div
-      {...props}
-      className={clsx(styles.card, styles.commitCard, props.className)}
-    >
-      <h2 className={styles.cardTitle}>Latest commit</h2>
-      <div className={styles.commitBranches}>
-        <div className={styles.commitDot} />
-        <span className={styles.commitBranch}>zyplos/quip</span>
-        <div className={styles.commitDot} />
-        <span className={styles.commitBranch}>main</span>
+    <HomeCard {...props} className={className} padding="m">
+      <SmallHeading>Latest commit</SmallHeading>
+      <div className={clsx(styles.flexRow, styles.infoRow)}>
+        <div className={clsx(styles.flexRow, styles.infoCell)}>
+          <Folder />
+          <span>zyplos/quip</span>
+        </div>
+        <div className={clsx(styles.flexRow, styles.infoCell)}>
+          <Branch />
+          <span>main</span>
+        </div>
       </div>
-      <p className={styles.commitMessage}>
-        Tweet: component now takes class names...
-      </p>
-      <p className={styles.commitTime}>2m ago</p>
-    </div>
+      <div className="textContent">
+        <p className={styles.commitMessage}>new commit component ui done</p>
+        <p className={styles.timestamp}>2m ago</p>
+      </div>
+    </HomeCard>
   );
 }
