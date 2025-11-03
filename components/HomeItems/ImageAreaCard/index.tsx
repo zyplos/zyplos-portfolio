83d885471122d9edbd2209fb0f6e86d15a82ede8
Angel <22824872+Zyplos@users.noreply.github.com>
2025-11-03 00:10:10 -0600

    main image card uses home card now

--- components/HomeItems/ImageAreaCard/index.tsx
+++ components/HomeItems/ImageAreaCard/index.tsx
@@ -1,14 +1,18 @@
 import clsx from "clsx";
+import { HomeCard } from "../HomeCard";
 import styles from "./styles.module.scss";
 
 interface ImageAreaCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export default function ImageAreaCard({ ...props }: ImageAreaCardProps) {
+export default function ImageAreaCard({
+  className,
+  ...props
+}: ImageAreaCardProps) {
   return (
-    <div {...props} className={clsx(styles.imageAreaCard, props.className)}>
+    <HomeCard {...props} className={clsx(styles.imageAreaCard, className)}>
       <div className={styles.imageArea}>
         <span className={styles.imageAreaText}>image area</span>
       </div>
-    </div>
+    </HomeCard>
   );
 }
