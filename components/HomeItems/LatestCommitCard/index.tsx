a20d0ed1e1f618a29ee02bfd90073efe368cbe30
Angel <22824872+Zyplos@users.noreply.github.com>
2025-11-03 17:31:12 -0600

    latest commit uses getGitHubActivity data, nicer styling

--- components/HomeItems/LatestCommitCard/index.tsx
+++ components/HomeItems/LatestCommitCard/index.tsx
@@ -1,33 +1,59 @@
-"use client";
-
 import clsx from "clsx";
 import { HomeCard, SmallHeading } from "../HomeCard";
 import styles from "./styles.module.scss";
 import Folder from "@/components/Icons/Folder";
 import Branch from "@/components/Icons/Branch";
+import getGitHubActivity from "@/internals/getGitHubActivity";
+import AnchorLink from "@/components/AnchorLink";
+import RelativeTime from "@/components/RelativeTime";
+import BlurIntoExistence from "@/components/BlurIntoExistence";
 
 interface LatestCommitCardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export default function LatestCommitCard({
+export default async function LatestCommitCard({
   className,
   ...props
 }: LatestCommitCardProps) {
+  const data = await getGitHubActivity();
+
+  if ("code" in data) {
+    return (
+      <HomeCard {...props} className={className} padding="m">
+        <SmallHeading>Latest commit</SmallHeading>
+        <div className="textContent">
+          <p className={styles.commitMessage}>Could not load commit data.</p>
+          <p className={styles.timestamp}>{data.message}</p>
+        </div>
+      </HomeCard>
+    );
+  }
+
   return (
-    <HomeCard {...props} className={className} padding="m">
+    <HomeCard
+      {...props}
+      className={clsx(styles.cardWrapper, className)}
+      padding="m"
+    >
       <SmallHeading>Latest commit</SmallHeading>
       <div className={clsx(styles.flexRow, styles.infoRow)}>
         <div className={clsx(styles.flexRow, styles.infoCell)}>
           <Folder />
-          <span>zyplos/quip</span>
+          <span>{data.repo}</span>
         </div>
         <div className={clsx(styles.flexRow, styles.infoCell)}>
           <Branch />
-          <span>main</span>
+          <span>{data.branch}</span>
         </div>
       </div>
-      <div className="textContent">
-        <p className={styles.commitMessage}>new commit component ui done</p>
-        <p className={styles.timestamp}>2m ago</p>
+      <div className={clsx("textContent", styles.commitMessageWrapper)}>
+        <div className={styles.commitMessage}>
+          <AnchorLink href={data.url} target="_blank">
+            <p>{data.message}</p>
+          </AnchorLink>
+        </div>
+        <BlurIntoExistence className={styles.timestamp}>
+          <RelativeTime timestamp={data.timestamp} />
+        </BlurIntoExistence>
       </div>
     </HomeCard>
   );
