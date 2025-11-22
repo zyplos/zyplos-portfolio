e48517c885430788f17961bb4f293bef5e8483f5
Angel <22824872+Zyplos@users.noreply.github.com>
2025-11-21 20:45:43 -0600

    gemini 3 pro: added material 3 switch component

---
+++ components/Icons/Check.tsx
@@ -0,0 +1,15 @@
+import type { SVGProps } from "@/internals/types";
+
+export default function Check({ ...props }: SVGProps) {
+  return (
+    // biome-ignore lint/a11y/noSvgWithoutTitle: decorative element
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="m382-354 339-339q12-12 28-12t28 12q12 12 12 28.5T777-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z" />
+    </svg>
+  );
+}
