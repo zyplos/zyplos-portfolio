020b04dafb19d6af10c8da16c2c90dfbab6385fa
Angel <22824872+Zyplos@users.noreply.github.com>
2026-01-07 15:42:26 -0600

    Slider: can take a class name for root element now

--- components/Slider/index.tsx
+++ components/Slider/index.tsx
@@ -1,15 +1,14 @@
-import clsx from "clsx";
-import styles from "./styles.module.scss";
 import {
   Slider as BaseSlider,
   type SliderRootProps,
 } from "@base-ui-components/react";
+import styles from "./styles.module.scss";
 
 interface SliderProps extends SliderRootProps {}
 
-export default function Slider({ ...props }: SliderProps) {
+export default function Slider({ className, ...props }: SliderProps) {
   return (
-    <BaseSlider.Root defaultValue={25} {...props}>
+    <BaseSlider.Root defaultValue={25} {...props} className={className}>
       <BaseSlider.Control className={styles.control}>
         <BaseSlider.Track className={styles.track}>
           <BaseSlider.Indicator className={styles.indicator} />
